10aff997e1ddeaf987bf8642ebaa3ccfb567c038
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-04-10 19:50:26 +0200

    Add more defs

--- src/lang/expr.rs
+++ src/lang/expr.rs
@@ -1,8 +1,12 @@
-use super::Stmt;
+use super::{Ident, Stmt, Type};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Expr {
     If(ExprIf),
+    Assign(ExprAssign),
+    Binary(ExprBinary),
+    Field(ExprField),
+    Call(ExprCall),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -10,9 +14,55 @@ pub struct ExprIf {
     pub cond: Box<Expr>,
     pub then_branch: Block,
     pub else_branch: Option<Box<Expr>>,
+    pub ty: Type,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ExprAssign {
+    pub left: Box<Expr>,
+    pub right: Box<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ExprBinary {
+    pub left: Box<Expr>,
+    pub op: BinOp,
+    pub right: Box<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ExprField {
+    pub base: Box<Expr>,
+    pub member: Member,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ExprCall {
+    pub func: Box<Expr>,
+    pub args: Vec<Expr>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Block {
     pub stmts: Vec<Stmt>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Eq,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Member {
+    Named(Ident),
+    Unnamed(Index),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Index {
+    pub index: u32,
 }
